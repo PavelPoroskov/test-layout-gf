5f4422dcd53fbaffd0d52ced70f6a25b8f7587c8
PavelPoroskov <pavl1853@gmail.com>
2019-08-14 02:54:14 +0300

    Updates

---
+++ precache-manifest.43678279e47b5a4520110dcd10af92a2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa82f04545cc3f5eeb8628d2d359c06",
+    "url": "/test-layout-gf/index.html"
+  },
+  {
+    "revision": "7b69a6dcbe843b39bf80",
+    "url": "/test-layout-gf/static/js/2.9f680e8d.chunk.js"
+  },
+  {
+    "revision": "8c1b5e3f592e006aab27",
+    "url": "/test-layout-gf/static/js/main.5299cf14.chunk.js"
+  },
+  {
+    "revision": "79ae30078f87c78e72c6",
+    "url": "/test-layout-gf/static/js/runtime~main.97494847.js"
+  }
+]);
